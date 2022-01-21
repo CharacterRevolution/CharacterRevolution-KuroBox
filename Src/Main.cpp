f7d6c68ac2fe223ade5bc5298d973ba66139cd34
LaoshuBaby <keaitianxinmail@qq.com>
2022-01-21 19:55:52 +0800

    计时器卡关了捏

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,12 +1,16 @@
 ﻿#include <iostream>
 #include <stdlib.h>
+#include <chrono>
 #include <time.h>
 
 using namespace std;
+// using namespace std::corono;  https://zhuanlan.zhihu.com/p/137626056  这C++11的新特性咋不支持捏
 
 int CHAR_PIXEL = 16;
 int CHAR_NUM_WIDTH = 40;
 int CHAR_NUM_HEIGHT = 20;
+
+int FRAME_RATE = 5; // 视情况增加到8，再窄不好除了，局刷我不知道怎么搞
 
 int main(int argc, char* argv[])
 {
@@ -43,7 +47,18 @@ int main(int argc, char* argv[])
 		cout << duration << endl;
 		cout << "全屏重绘" << endl;
 		cout << "当前容许帧率为" << (double)1000 / duration << "  fps" << endl;
-		system("pause"); //按理说如果是删掉就是最大帧率来整了，接下来考虑用计时器代替手动打断或者完全不打断，不打断纯自己无限刷的话效果相当惊人
+		bool flag_timeout = false;//所以我怎么整计时器呢？
+		bool flag_userinput = false;//Keyboard or mouse Input, "system("pause")" 算用户打断
+		bool flag_allow_global_refresh = false;
+		do
+		{
+			if (flag_timeout == true || flag_userinput == false)
+			{
+				flag_allow_global_refresh = true;
+			}
+		} while (flag_allow_global_refresh == false);
+
+		//按理说如果是删掉就是最大帧率来整了，接下来考虑用计时器代替手动打断或者完全不打断，不打断纯自己无限刷的话效果相当惊人
 	}
 	
 	
