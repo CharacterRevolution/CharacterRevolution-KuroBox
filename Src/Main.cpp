67d68e01e2a9ef407a1100037ad2fca26af06873
LaoshuBaby <keaitianxinmail@qq.com>
2022-01-21 11:39:00 +0800

    开始引入像素概念

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+int CHAR_PIXEL = 16;
+int CHAR_NUM_X = 40;
+int CHAR_NUM_Y = 20;
+
 int main(int argc, char* argv[])
 {
-	cout << "你好，世界" << endl;
-	cout << "Powered by 字魂凝创游戏" << endl;
+	int PIXEL_X = CHAR_NUM_X * CHAR_PIXEL;
+	int PIXEL_Y = CHAR_NUM_Y * CHAR_PIXEL;
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < PIXEL_X; ++i)
+	{
+		for (j = 0; j < PIXEL_Y; ++j)
+		{
+			cout << "■" /*<< endl*/;
+		}
+		cout << endl;
+	}
 	return 0;
 }
