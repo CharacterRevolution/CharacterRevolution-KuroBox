dda6ad6f837f5368efa4dd562541b3cae4ee4ca8
LaoshuBaby <keaitianxinmail@qq.com>
2022-01-21 14:00:28 +0800

    消歧义，准备主循环

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,26 +1,32 @@
 ﻿#include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int CHAR_PIXEL = 16;
-int CHAR_NUM_X = 40/2;
-int CHAR_NUM_Y = 20/2;
+int CHAR_NUM_WIDTH = 40;
+int CHAR_NUM_HEIGHT = 20;
 
 int main(int argc, char* argv[])
 {
-	int PIXEL_X = CHAR_NUM_X * CHAR_PIXEL; //绘制像素模拟到现在的屏幕才会用到
-	int PIXEL_Y = CHAR_NUM_Y * CHAR_PIXEL; //绘制像素模拟到现在的屏幕才会用到
+	int PIXEL_WIDTH = CHAR_NUM_WIDTH * CHAR_PIXEL; //绘制像素模拟到现在的屏幕才会用到
+	int PIXEL_HEIGHT = CHAR_NUM_HEIGHT * CHAR_PIXEL; //绘制像素模拟到现在的屏幕才会用到
 	int i = 0;
 	int j = 0;
-	for (i = 0; i < CHAR_NUM_X; ++i)
+	//while (1)
+	//{
+	//	;
+	//}
+	system("cls");
+	for (i = 0; i < CHAR_NUM_HEIGHT; ++i)
 	{
-		for (j = 0; j < CHAR_NUM_Y; ++j)
+		for (j = 0; j < CHAR_NUM_WIDTH; ++j)
 		{
-			if (i == 0 || i == CHAR_NUM_X-1)
+			if (i == 0 || i == CHAR_NUM_HEIGHT -1)
 			{
 				cout << "■";
 			}
-			else if (j == 0 || j == CHAR_NUM_Y-1)
+			else if (j == 0 || j == CHAR_NUM_WIDTH -1)
 			{
 				cout << "■";
 			}
@@ -31,5 +37,6 @@ int main(int argc, char* argv[])
 		}
 		cout << endl;
 	}
+	system("pause");
 	return 0;
 }
