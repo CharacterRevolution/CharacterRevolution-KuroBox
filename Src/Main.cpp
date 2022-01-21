ae51e3bfb6a4c0aa54e7d4b8b3d55ca633c674c9
LaoshuBaby <keaitianxinmail@qq.com>
2022-01-21 20:52:36 +0800

    不写了！（摔！）

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -10,7 +10,7 @@ int CHAR_PIXEL = 16;
 int CHAR_NUM_WIDTH = 40;
 int CHAR_NUM_HEIGHT = 20;
 
-int FRAME_RATE = 5; // 视情况增加到8，再窄不好除了，局刷我不知道怎么搞
+int FRAME_RATE = 1; // 视情况增加到8，再窄不好除了，局刷我不知道怎么搞
 
 int main(int argc, char* argv[])
 {
@@ -21,7 +21,9 @@ int main(int argc, char* argv[])
 	while (1)
 	{
 		clock_t start, stop;
+		clock_t start_tmp, stop_tmp;
 		start = clock();
+		start_tmp = start;
 		system("cls");
 		for (i = 0; i < CHAR_NUM_HEIGHT; ++i)
 		{
@@ -47,12 +49,20 @@ int main(int argc, char* argv[])
 		cout << duration << endl;
 		cout << "全屏重绘" << endl;
 		cout << "当前容许帧率为" << (double)1000 / duration << "  fps" << endl;
-		bool flag_timeout = false;//所以我怎么整计时器呢？
+		bool flag_timeout = false;
 		bool flag_userinput = false;//Keyboard or mouse Input, "system("pause")" 算用户打断
+		bool flag_waitmode = true;//对于用户无操作的情况进行判定，不操作的话就不刷新
 		bool flag_allow_global_refresh = false;
+
 		do
 		{
-			if (flag_timeout == true || flag_userinput == false)
+			stop_tmp = clock();
+			int duration_tmp = stop_tmp - start_tmp;
+			if (duration_tmp >= 1000 / FRAME_RATE)
+			{
+				flag_timeout = true; //每个循环检查一次以模拟定时器
+			}
+			if ((flag_timeout == true || flag_userinput == true)&& flag_waitmode==false)
 			{
 				flag_allow_global_refresh = true;
 			}
