2a0395397c0f2b013e93cd5a83e801f03853deba
LaoshuBaby <keaitianxinmail@qq.com>
2022-01-21 11:43:05 +0800

    画框
    边框有两个问题
    1. 差点把PIXEL_X写进去，横行爆炸
    2. 不要忘记-1

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -3,20 +3,31 @@
 using namespace std;
 
 int CHAR_PIXEL = 16;
-int CHAR_NUM_X = 40;
-int CHAR_NUM_Y = 20;
+int CHAR_NUM_X = 40/2;
+int CHAR_NUM_Y = 20/2;
 
 int main(int argc, char* argv[])
 {
-	int PIXEL_X = CHAR_NUM_X * CHAR_PIXEL;
-	int PIXEL_Y = CHAR_NUM_Y * CHAR_PIXEL;
+	int PIXEL_X = CHAR_NUM_X * CHAR_PIXEL; //绘制像素模拟到现在的屏幕才会用到
+	int PIXEL_Y = CHAR_NUM_Y * CHAR_PIXEL; //绘制像素模拟到现在的屏幕才会用到
 	int i = 0;
 	int j = 0;
-	for (i = 0; i < PIXEL_X; ++i)
+	for (i = 0; i < CHAR_NUM_X; ++i)
 	{
-		for (j = 0; j < PIXEL_Y; ++j)
+		for (j = 0; j < CHAR_NUM_Y; ++j)
 		{
-			cout << "■" /*<< endl*/;
+			if (i == 0 || i == CHAR_NUM_X-1)
+			{
+				cout << "■";
+			}
+			else if (j == 0 || j == CHAR_NUM_Y-1)
+			{
+				cout << "■";
+			}
+			else
+			{
+				cout << "□";
+			}
 		}
 		cout << endl;
 	}
